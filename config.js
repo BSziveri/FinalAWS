8930a8bf1f0b4c6571cf54f2740d360d49908b3b
Balazs Sziveri <78025475+BSziveri@users.noreply.github.com>
2022-04-19 15:04:05 +0100

    updatingjs

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: ' eu-west-1_SnwfUktVA', 
+        userPoolClientId: '3mj23gl8clcphrom8hi6um79ev', 
+        region: 'eu-west-1' 
+    },
+    api: {
+        invokeUrl: ' https://v5rqbyd6q4.execute-api.eu-west-1.amazonaws.com/CloudTaxiSite'
+    }
+};
